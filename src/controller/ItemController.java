21daac5a050c9ee4f243bd3807847687341c3113
USER <udaraj08@gmail.com>
2021-10-09 11:25:03 +0530

    item ui is created.

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -7,5 +7,35 @@
 
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class ItemController {
+    public TextField txtItemId;
+    public TextField txtItemeName;
+    public TextField txtItemPrice;
+    public TextField txtQtyOnHand;
+    public Button btnCreateItem;
+    public ImageView btnBack;
+
+    public void txtItemIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtItemeNameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtItemPriceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtQtyOnHandOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCreateItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(MouseEvent mouseEvent) {
+    }
 }
